59f1af6e335334ee7ba6fbdb6da8c0a7111b6ecd
macjjuni <macjjuni@gmail.com>
2024-11-04 16:03:19 +0900

    feature. date 함수 작성 및 테스트 코드 작성
    - getCurrentDate
    - getFormatDate
    - calcDate
    - calcCurrentDateDifference

--- src/__tests__/date.test.ts
+++ src/__tests__/date.test.ts
@@ -1,18 +1,210 @@
 import {date} from '@/index';
 import dayjs from 'dayjs';
 
-const format = date.formatConfig;
-
+const {formatConfig: format, getCurrentDate, getFormatDate, calcDate, calcCurrentDateDifference} = date;
 
 describe('date util test', () => {
 
-    it('getCurrentNow function returns the current date in the correct format', () => {
-        const expectedCurrentDate = dayjs().format(format.detail);
-        const currentDate = date.getCurrentDate(format.detail);
 
-        expect(expectedCurrentDate).toBe(currentDate);
+    describe('getCurrentNow', () => {
+
+        it('getCurrentNow function returns the current date in the correct format', () => {
+            const expectedCurrentDate = dayjs().format(format.detail);
+            const currentDate = getCurrentDate(format.detail);
+
+            expect(expectedCurrentDate).toBe(currentDate);
+        });
+
+        it('getCurrentNow function with no params returns the current date in the correct format', () => {
+            const expectedCurrentDate = dayjs().format(format.detail);
+            const currentDate = getCurrentDate();
+
+            expect(expectedCurrentDate).toBe(currentDate);
+        });
+
     });
 
 
+    describe('getFormatDate', () => {
+        it('formats the current date correctly according to the provided format', () => {
+            const currentDate = dayjs().format();
+            const expectedDate = dayjs(currentDate).format(format.detail);
+            const targetDate = getFormatDate(currentDate, format.detail);
 
+            expect(expectedDate).toBe(targetDate);
+        });
+
+        it('formats the specific date correctly according to the provided format', () => {
+            const specificDate = '2024-12-12';
+            const expectedDate = dayjs(specificDate).format(format.default);
+            const targetDate = getFormatDate(specificDate, format.default);
+
+            expect(expectedDate).toBe(targetDate);
+        });
+
+        it('formats the current date correctly for multiple formats', () => {
+            const firstSpecificFormat = 'dddd, MMMM D, YYYY';
+            const secondSpecificFormat = 'MM/DD/YYYY';
+            const currentDate = dayjs().format();
+            const expectedDate1 = dayjs(currentDate).format(firstSpecificFormat);
+            const targetDate1 = getFormatDate(currentDate, firstSpecificFormat);
+            const expectedDate2 = dayjs(currentDate).format(secondSpecificFormat);
+            const targetDate2 = getFormatDate(currentDate, secondSpecificFormat);
+
+            expect(expectedDate1).toBe(targetDate1);
+            expect(expectedDate2).toBe(targetDate2);
+        });
+    });
+
+
+    describe('calcDate', () => {
+        it('should calculate the date correctly by adding days', () => {
+            const currentDate = dayjs().format();
+            const targetFormat = format.detail;
+            const targetUnit = 'day';
+            const targetAmount = 7;
+            const expectedDate = calcDate(currentDate, targetAmount, targetUnit, targetFormat);
+            const targetDate = dayjs(currentDate).add(targetAmount, targetUnit).format(targetFormat);
+
+            expect(expectedDate).toBe(targetDate);
+        });
+
+        it('should calculate the date correctly by subtracting days', () => {
+            const currentDate = dayjs().format();
+            const targetFormat = format.detail;
+            const targetUnit = 'day';
+            const targetAmount = -5;
+            const expectedDate = calcDate(currentDate, targetAmount, targetUnit, targetFormat);
+            const targetDate = dayjs(currentDate).add(targetAmount, targetUnit).format(targetFormat);
+
+            expect(expectedDate).toBe(targetDate);
+        });
+
+        it('should calculate the date correctly by adding months', () => {
+            const currentDate = dayjs().format();
+            const targetFormat = format.detail;
+            const targetAmount = 2;
+            const targetUnit = 'month';
+            const expectedDate = dayjs(currentDate).add(targetAmount, targetUnit).format(targetFormat);
+            const actualDate = calcDate(currentDate, targetAmount, targetUnit, targetFormat);
+
+            expect(actualDate).toBe(expectedDate);
+        });
+
+        it('should calculate the date correctly by subtracting months', () => {
+            const currentDate = dayjs().format();
+            const targetFormat = format.detail;
+            const targetAmount = -3;
+            const targetUnit = 'month';
+            const expectedDate = dayjs(currentDate).add(targetAmount, targetUnit).format(targetFormat);
+            const actualDate = calcDate(currentDate, targetAmount, targetUnit, targetFormat);
+
+            expect(actualDate).toBe(expectedDate);
+        });
+
+
+        it('should calculate the date correctly by adding years', () => {
+            const currentDate = dayjs().format();
+            const targetFormat = format.detail;
+            const targetAmount = 1;
+            const targetUnit = 'year';
+            const expectedDate = dayjs(currentDate).add(targetAmount, targetUnit).format(targetFormat);
+            const actualDate = calcDate(currentDate, targetAmount, targetUnit, targetFormat);
+
+            expect(actualDate).toBe(expectedDate);
+        });
+
+        it('should calculate the date correctly by subtracting years', () => {
+            const currentDate = dayjs().format();
+            const targetFormat = format.detail;
+            const targetAmount = -1;
+            const targetUnit = 'year';
+            const expectedDate = dayjs(currentDate).add(targetAmount, targetUnit).format(targetFormat);
+            const actualDate = calcDate(currentDate, targetAmount, targetUnit, targetFormat);
+
+            expect(actualDate).toBe(expectedDate);
+        });
+    });
+
+    describe('calcCurrentDateDifference', () => {
+        it('should calculate the difference in seconds correctly', () => {
+            const targetDate = dayjs().add(30, 'second').toISOString(); // 30초 후
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'second');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'second');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should calculate the difference in minutes correctly', () => {
+            const targetDate = dayjs().add(5, 'minute').toISOString(); // 현재 시간에서 5분 후
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'minute');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'minute');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should calculate the difference in hours correctly', () => {
+            const targetDate = dayjs().add(3, 'hour').toISOString(); // 현재 시간에서 3시간 후
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'hour');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'hour');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should calculate the difference in days correctly', () => {
+            const targetDate = dayjs().add(10, 'day').toISOString(); // 현재 시간에서 10일 후
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'day');
+            const actualDifference = calcCurrentDateDifference(targetDate);
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should calculate the difference in months correctly', () => {
+            const targetDate = dayjs().add(2, 'month').toISOString(); // 현재 시간에서 2개월 후
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'month');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'month');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should calculate the difference in years correctly', () => {
+            const targetDate = dayjs().add(12, 'year').toISOString(); // 현재 시간에서 1년 후
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'year');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'year');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should return a negative difference for past dates in seconds', () => {
+            const targetDate = dayjs().subtract(10, 'second').toISOString(); // 현재 시간에서 10초 전
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'second');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'second');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should return a negative difference for past dates in minutes', () => {
+            const targetDate = dayjs().subtract(2, 'minute').toISOString(); // 현재 시간에서 2분 전
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'minute');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'minute');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should return a negative difference for past dates in hours', () => {
+            const targetDate = dayjs().subtract(1, 'hour').toISOString(); // 현재 시간에서 1시간 전
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'hour');
+            const actualDifference = calcCurrentDateDifference(targetDate, 'hour');
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+
+        it('should return a negative difference for past dates in days', () => {
+            const targetDate = dayjs().subtract(5, 'day').toISOString(); // 현재 시간에서 5일 전
+            const expectedDifference = dayjs(targetDate).diff(dayjs(), 'day');
+            const actualDifference = calcCurrentDateDifference(targetDate);
+
+            expect(actualDifference).toBe(expectedDifference);
+        });
+    });
 });
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,20 +1,54 @@
 import 'dayjs/locale/ko';
 import dayjs from 'dayjs';
 
+type DateType = Date | string | number;
+type CalculateUnitType = 'second' | 'minute' | 'hour' | 'day' | 'month' | 'year';
+
 dayjs.locale('ko'); // initialize Locate
 
 const formatConfig = {
     default: 'YYYY.MM.DD',
-    detail: 'YYYY-MM-DD HH:mm',
+    detail: 'YYYY-MM-DD HH:mm:ss',
 };
 
-function getCurrentDate(format: string | undefined = formatConfig.detail) {
-    return dayjs().format(format);
+function getCurrentDate(format?: string) {
+    return dayjs().format(format || formatConfig.detail);
 }
 
+function getFormatDate(date: DateType, format?: string) {
+    return dayjs(date).format(format || formatConfig.default);
+}
 
+function calcDate(date: DateType, amount: number, unit: CalculateUnitType = 'day', format?: string) {
+    const calculatedDate = dayjs(date).add(amount, unit);
+    return calculatedDate.format(format);
+}
+
+function calcCurrentDateDifference(date: DateType, unit: CalculateUnitType = 'day') {
+    const currentDateTimestamp = dayjs().valueOf();
+    const targetDateTimestamp = dayjs(date).valueOf();
+
+    const differenceInMilliseconds = targetDateTimestamp - currentDateTimestamp;
+
+    switch (unit) {
+        case 'second':
+            return Math.floor(differenceInMilliseconds / 1000); // 초
+        case 'minute':
+            return Math.floor(differenceInMilliseconds / (1000 * 60)); // 분
+        case 'hour':
+            return Math.floor(differenceInMilliseconds / (1000 * 60 * 60)); // 시
+        case 'day':
+            return Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24)); // 일
+        case 'month':
+            return dayjs(date).diff(dayjs(), 'month'); // 월
+        case 'year':
+            return dayjs(date).diff(dayjs(), 'year'); // 연
+        default:
+            throw new Error('Invalid unit specified'); // 에러
+    }
+};
 
 
 export default {
-    formatConfig, getCurrentDate,
+    formatConfig, getCurrentDate, getFormatDate, calcDate, calcCurrentDateDifference,
 };
